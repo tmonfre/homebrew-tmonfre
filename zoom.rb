a28d1f0c4311921baf59ca82670f31659cafce1e
Thomas Monfre <tmonfre1@gmail.com>
2020-06-10 15:58:40 -0500

    switch back to binary file

--- zoom.rb
+++ zoom.rb
@@ -1,13 +1,13 @@
 class Zoom < Formula
     desc "Command line tool for launching and saving recurring Zoom meetings"
     homepage "https://github.com/tmonfre/zoom-cli"
-    url "http://tmonfre.herokuapp.com/fs/file/zoom.dll"
-    sha256 "c2914e816c116cffc6f2d05a3b76e9a5e348d29c09b88bbd78c04066c85d31dd"
+    url "http://tmonfre.herokuapp.com/fs/file/zoom"
+    sha256 "981714be5d379506b39fd04ba3334b5e6d8f66104d1290a8a780f2b7e90ace28"
     version "1.0.0"
     
     bottle :unneeded
   
     def install
-      bin.install "zoom.dll"
+      bin.install "zoom"
     end
   end
