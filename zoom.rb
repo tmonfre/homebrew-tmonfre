4b013ac5d59639cb6548e37146aa2a7e0dacacf4
Thomas Monfre <tmonfre1@gmail.com>
2020-06-10 15:54:20 -0500

    switch to zoom.dll

--- zoom.rb
+++ zoom.rb
@@ -1,7 +1,7 @@
 class Zoom < Formula
     desc "Command line tool for launching and saving recurring Zoom meetings"
     homepage "https://github.com/tmonfre/zoom-cli"
-    url "http://tmonfre.herokuapp.com/fs/file/zoom"
+    url "http://tmonfre.herokuapp.com/fs/file/zoom.dll"
     sha256 "981714be5d379506b39fd04ba3334b5e6d8f66104d1290a8a780f2b7e90ace28"
     version "1.0.0"
     
