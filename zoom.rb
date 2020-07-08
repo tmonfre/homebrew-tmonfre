a10bf6039e9eba21a578b0fe930aa01070fb17e9
Thomas Monfre <tmonfre1@gmail.com>
2020-07-08 17:43:36 -0500

    update sha256

--- zoom.rb
+++ zoom.rb
@@ -2,7 +2,7 @@ class Zoom < Formula
     desc "Command line tool for launching and saving recurring Zoom meetings"
     homepage "https://github.com/tmonfre/zoom-cli"
     url "http://tmonfre.herokuapp.com/fs/file/zoom.tar.gz"
-    sha256 "30762f3d19fdaa8632f91cd23cb9650abdd3f3c3699804508001f669cd21c899"
+    sha256 "4ad76242fa6d59601ab07478cf38e435b05e8a7f79cc4b08408ee1d146c42f1a"
     version "1.0.1"
     
     bottle :unneeded
