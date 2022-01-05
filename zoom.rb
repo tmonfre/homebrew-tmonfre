6c09f64eae677e8aa14c1aa98305793565b63601
Thomas Monfre <tmonfre1@gmail.com>
2022-01-05 13:32:32 -0600

    update zoom to new build

--- zoom.rb
+++ zoom.rb
@@ -1,14 +1,13 @@
 class Zoom < Formula
     desc "Command line tool for launching and saving recurring Zoom meetings"
     homepage "https://github.com/tmonfre/zoom-cli"
-    url "http://tmonfre.herokuapp.com/fs/file/zoom.tar.gz"
-    sha256 "4ad76242fa6d59601ab07478cf38e435b05e8a7f79cc4b08408ee1d146c42f1a"
-    version "1.0.1"
+    url "https://thomasmonfre.com/zoom.tar.gz"
+    sha256 "05ec04099fae91efb80208ba08dbd2a911ca0b1f82ad6b95772d1bb371ca22ce"
+    version "1.1.0"
     
     bottle :unneeded
   
     def install
       bin.install "zoom"
-      bin.install ".store"
     end
   end
