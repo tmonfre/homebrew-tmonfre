bca89e97a637804a27d0dcb92a288aa35e6e428f
Thomas Monfre <tmonfre1@gmail.com>
2022-01-05 15:15:39 -0600

    update zoom hash +  install instructions

--- zoom.rb
+++ zoom.rb
@@ -2,10 +2,11 @@ class Zoom < Formula
     desc "Command line tool for launching and saving recurring Zoom meetings"
     homepage "https://github.com/tmonfre/zoom-cli"
     url "https://thomasmonfre.com/zoom.tar.gz"
-    sha256 "b600db0d28f0bf30e5350c2990873711f1c3ffece347e8ce548493919c60b8f1"
+    sha256 "04c08987026b334be949e5f383955875295ae1c9d999e81cead7befee8e7f0e6"
     version "1.1.1"
       
     def install
       bin.install "zoom"
+      bin.install "zoom/*"
     end
   end
