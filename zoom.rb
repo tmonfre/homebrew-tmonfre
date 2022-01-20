2fc33854e310dfb09bebb41a50bd989d1876ff33
Thomas Monfre <tmonfre1@gmail.com>
2022-01-19 22:49:08 -0600

    chore: bump zoom to v1.1.5

--- zoom.rb
+++ zoom.rb
@@ -2,7 +2,7 @@ class Zoom < Formula
     desc "Command line tool for launching and saving recurring Zoom meetings"
     homepage "https://github.com/tmonfre/zoom-cli"
     url "https://thomasmonfre.com/zoom.tar.gz"
-    sha256 "0cb8ce77b1683863065ce626a1357dab42497a8946b3b66b8d171fb5dea30e51"
+    sha256 "b9cd3836e417565a18076c34bd08557a89f3da891fda9b00cb4740adc7c80b0a"
     version "1.1.5"
       
     def install
