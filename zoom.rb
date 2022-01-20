116cbe5ac7f3bfb8ebd702471df6550276433924
Thomas Monfre <tmonfre1@gmail.com>
2022-01-20 14:59:16 -0600

    chore: change url for zoom v1.1.6

--- zoom.rb
+++ zoom.rb
@@ -1,7 +1,7 @@
 class Zoom < Formula
     desc "Command line tool for launching and saving recurring Zoom meetings"
     homepage "https://github.com/tmonfre/zoom-cli"
-    url "https://thomasmonfre.com/zoom.tar.gz"
+    url "https://github.com/tmonfre/zoom-cli/releases/download/v1.1.6/zoom.tar.gz"
     sha256 "2100e1c6a7d7263c99137817848354cb4b20295b49616be83b05836eacfa932e"
     version "1.1.6"
       
